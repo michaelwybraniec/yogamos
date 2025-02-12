e2171118aa268126a4be2bdd065b5d37c1aca245
Michael Wybraniec <michaelwybraniec@me.com>
2025-02-12 16:15:33 +0100

    init 2

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import type { NavItem } from '@nuxt/content'
+
+const navigation = inject<Ref<NavItem[]>>('navigation', ref([]))
+
+const links = [
+  {
+    label: 'Docs',
+    to: '/docs',
+  },
+  {
+    label: 'Blog',
+    to: '/blog',
+  },
+  {
+    label: 'Pricing',
+    to: '/pricing',
+  },
+]
+</script>
+
+<template>
+  <UHeader :links="links">
+    <template #logo> YOGAMOS <UBadge label="ES" variant="subtle" class="mb-0.5" /> </template>
+
+    <template #right>
+      <UButton label="Sign in" color="gray" to="/login" />
+      <UButton
+        label="Sign up"
+        icon="i-heroicons-arrow-right-20-solid"
+        trailing
+        color="black"
+        to="/signup"
+        class="hidden lg:flex"
+      />
+    </template>
+
+    <template #panel>
+      <UNavigationTree :links="mapContentNavigation(navigation)" default-open />
+    </template>
+  </UHeader>
+</template>
