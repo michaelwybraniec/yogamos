5574c48e1180e25720dc3171040c11dcfcb75b4a
Michael Wybraniec <46869143+michaelwybraniec@users.noreply.github.com>
2025-03-26 10:57:37 -0500

    Dev (#9)
    * removing deploy, updating studio.yml to dev branch
    
    * removing deploy, updating studio.yml to dev branch
    
    * Blog and yml
    
    * eslint should run on PRs merging into dev as well
    
    * eslint fix
    
    * eslint config fix
    
    * Ensures pnpm is installed properly using corepack, which is recommended for Node.js.
    
    * This checks if pnpm is installed before proceeding.
    
    * Ensures Corepack is available (Corepack manages pnpm, yarn, etc.).
    
    * explicitly install pnpm via npm
    
    * debuging eslint
    
    * npm runs pnpm
    
    * Uses curl -fsSL https://get.pnpm.io/install.sh | sh to install pnpm.
    
    * Manually enables corepack
    
    * path issue cdn
    
    * action debug
    
    * one more..
    
    * eslint corepack enabled
    
    * verify pnpm installation
    
    * install pnpm
    
    * switching to yarn
    
    * updated to node 20
    
    * Create codeql.yml
    
    * Set Nuxt UI Pro License
    
    * Set Nuxt UI Pro License
    
    * eslitn permisions and node git ignore
    
    * permissions: checks: write
    
    * match rulset check name
    
    * Run ESLint

--- app/app.config.ts
+++ app/app.config.ts
@@ -6,6 +6,15 @@ export default defineAppConfig({
       primary: 'red',
       neutral: 'stone'
     },
+    blogPosts: {
+      base: 'flex flex-col gap-8 lg:gap-y-16',
+      variants: {
+        orientation: {
+          horizontal: 'sm:grid sm:grid-cols-2 lg:grid-cols-6',
+          vertical: ''
+        }
+      }
+    },
     button: {
       slots: {
         // base: 'font-bold'
